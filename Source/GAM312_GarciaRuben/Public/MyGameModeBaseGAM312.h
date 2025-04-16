ea20d1038c86e49ef4998428b871140c21b930f9
BluRonix <blue14madthunder@gmail.com>
2025-04-16 00:28:00 -0500

    Module1

---
+++ Source/GAM312_GarciaRuben/Public/MyGameModeBaseGAM312.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyGameModeBaseGAM312.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAM312_GARCIARUBEN_API AMyGameModeBaseGAM312 : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/GAM312_GarciaRuben/ghgj.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ghgj.h"
+
+ghgj::ghgj()
+{
+}
+
+ghgj::~ghgj()
+{
+}
