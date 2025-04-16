9854895188584739d1c45f91e574b1d65303ba2b
BluRonix <blue14madthunder@gmail.com>
2025-04-15 23:12:02 -0500

    More changes upon set up

---
+++ GAM312_GarciaRuben/Source/GAM312_GarciaRuben/GAM312_GarciaRuben.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAM312_GarciaRuben.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAM312_GarciaRuben, "GAM312_GarciaRuben" );
--- README.md
+++
@@ -1,2 +0,0 @@
-# GAM312_RubenGarcia_Redone
-Modules 1-6 to be redone. Here's the process.
